4e5a3261685c889fd44bac07109f7c35fdcdf9be
MinHien-git <64275316+MinHien-git@users.noreply.github.com>
2022-04-26 22:02:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,14 @@ const observer = new IntersectionObserver(entries => {
 hightlights.forEach(hightlight => {
     observer.observe(hightlight)
 })
+
+window.addEventListener('scroll',() => {
+    let value = window.scrollY
+    bgColumn.style.top = value*1.05 +'px'
+    frontColumnHood.style.top = value*0.5 +'px'
+    column.style.top = value*0.2 +'px'
+    frontColumn.style.top = value* 0 +'px'
+    productTitle.style.marginTop = value * 0.2 +'px'
+})
+
+burgerbtn.addEventListener("click",() => navitems.classList.toggle("active"))
